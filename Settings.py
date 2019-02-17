6f5f810acfbbf4eca3d671610769715cf35c1369
JORGE ERNESTO MARTIN PULIDO <je.martin@ISIS4426-0105.sis.virtual.uniandes.edu.co>
2019-02-16 22:46:08 -0500

    Cambia de Settings

--- Settings.py
+++ Settings.py
@@ -2,8 +2,8 @@ import os
 
 MEDIA_DIR = '../contests/Grupo02/supervoices/contests/media/'
 MEDIA_COMPLETED_DIR = '../contests/Grupo02/supervoices/contests/media/completed'
-HOST = '157.253.92.30'
-PORT = '5000'
+HOST = '172.24.42.32'
+PORT = '5432'
 DATABASE = "contests",
 USER = "contests",
 PASSWORD = "Admin.01"
