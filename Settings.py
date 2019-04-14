9bc5ffa86bfa107103352ee32b227dc613710253
jmartipu <35982396+jmartipu@users.noreply.github.com>
2019-04-14 18:01:15 -0500

    Update Settings.py
    Se actualizan los settings para enviar correos por defecto

--- Settings.py
+++ Settings.py
@@ -19,5 +19,5 @@ AWS_STORAGE_BUCKET_NAME_S3 = os.environ.get('AWS_STORAGE_BUCKET_NAME_S3')
 AWS_ACCESS_KEY_ID_S3 = os.environ.get('AWS_ACCESS_KEY_ID_S3') #os.environ.get('AWS_ACCESS_KEY_ID_S3')
 AWS_SECRET_ACCESS_KEY_S3 = os.environ.get('AWS_SECRET_ACCESS_KEY_S3') # os.environ.get('AWS_SECRET_ACCESS_KEY_S3')
 AWS_QUEUE_URL = 'https://us-east-2.queue.amazonaws.com/942770429245/supervoices_sqs.fifo'
-EMAIL_SEND = 'N'
+EMAIL_SEND = 'S'
 SLEEP_TIME = 2
